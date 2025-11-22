110c63985c8b601b42343dbc0cf1907bbcdadef2
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-22 23:22:41 +0000

    display weather info

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,3 +1,5 @@
+const weatherInfo = document.querySelector(".weather-info");
+
 const getWeather = async (e) => {
   e.preventDefault();
   const city = document.querySelector("#search-bar").value;
@@ -11,4 +13,13 @@ const getWeather = async (e) => {
   if (!res.ok) {
     throw new Error("Failed to get Weather");
   }
+
+  weatherInfo.innerHTML = `
+          <h2>${data.name}</h2>
+          <p><strong>${data.main.temp}°C</strong></p>
+          <p>${data.weather[0].description}</p>
+          <p>${data.main.temp_min}°/${data.main.temp_max}°C</p>
+          <p>Humidity: ${data.main.humidity}%</p>
+          <p>Wind Speed: ${data.wind.speed} m/s</p>
+          `;
 };
