735be26ccb1e44344f20b148f59b19360975a97a
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-22 01:55:31 +0000

    restructure project

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(`${__dirname}/public`));
 
 app.get("/weather", async (req, res) => {
   const city = req.query.city;
   try {
-    const data = await fetch(
+    const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=metric`
-    ).json();
-
+    );
+    const data = await response.json();
     if (data.cod !== 200) return res.status(404).json({ error: "City Not Found" });
 
     res.json(data);
