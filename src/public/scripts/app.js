03ee9c5919ece80e235a3fa9cad334e28dc1c1b3
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-23 21:23:23 +0000

    round numbers and minor changes

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -8,9 +8,9 @@ const windSpeed = document.querySelector("#windSpeed");
 
 const updateWeather = (data) => {
   city.textContent = `${data.name}, ${data.sys.country}`;
-  temp.textContent = `${data.main.temp}°C`;
+  temp.textContent = `${Math.round(data.main.temp)}°`;
   description.textContent = `${data.weather[0].description}`;
-  minMax.textContent = `${data.main.temp_min}°/${data.main.temp_max}°C`;
+  minMax.textContent = `${Math.round(data.main.temp_min)}°/${Math.round(data.main.temp_max)}°C`;
   humidity.textContent = `${data.main.humidity}%`;
   windSpeed.textContent = `${data.wind.speed} m/s`;
 };
