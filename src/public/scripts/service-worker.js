205cee30ba22917dded3323570a7412033580fcf
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-24 23:37:39 +0000

    added manifest to assets

--- src/public/scripts/service-worker.js
+++ src/public/scripts/service-worker.js
@@ -2,7 +2,14 @@ const { response } = require("express");
 
 const CACHE_NAME = "weather-cache-v1";
 
-const ASSETS = ["/", "/index.html", "/styles/style.css", "/images/icon-192x192.png", "/scripts/app.js"];
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles/style.css",
+  "/images/icon-192x192.png",
+  "/scripts/app.js",
+  "/manifest.json",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
