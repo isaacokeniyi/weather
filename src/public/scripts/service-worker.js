f2ad36560f67d61d6ac2c2eed0032bc9a97fe57a
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-24 23:31:16 +0000

    service worker

---
+++ src/public/scripts/service-worker.js
@@ -0,0 +1,25 @@
+const { response } = require("express");
+
+const CACHE_NAME = "weather-cache-v1";
+
+const ASSETS = ["/", "/index.html", "/styles/style.css", "/images/icon-192x192.png", "/scripts/app.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
