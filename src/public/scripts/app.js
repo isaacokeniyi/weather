6fd29e740ae1506ad66861d14b92b8c877854060
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-24 23:50:43 +0000

    registered service worker

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -36,7 +36,18 @@ const getWeather = async (e) => {
   updateWeather(data);
 };
 
-window.onload = async () => {
+window.addEventListener("load", async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("/service-worker.js");
+      console.log("Service worker registered");
+    } catch (error) {
+      console.error("Service worker registration failed:", err);
+    }
+  }
+});
+
+window.addEventListener("load", async () => {
   const city = localStorage.getItem("city");
   if (city) {
     const res = await fetch(`/weather?city=${city}`);
@@ -51,4 +62,4 @@ window.onload = async () => {
 
     updateWeather(data);
   }
-};
+});
